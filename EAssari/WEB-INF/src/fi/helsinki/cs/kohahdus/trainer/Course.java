e80367bcf9bffab8a4b59ca18d57c587c56dea5a
Taro Morimoto <morimoto@cs.helsinki.fi>
2006-10-12 09:02:05 +0000

    Added getters and setters and toString methods

--- EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/Course.java
+++ EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/Course.java
@@ -3,6 +3,60 @@ package fi.helsinki.cs.kohahdus.trainer;
 
 public class Course {
 	
+    private int modules = 0;                     // number of modules
+    private String courseID = null;              //  unique course identifier
+    private String courseName = null;            // 
+    private String courseMetadata = null;        // 
+    private String courseLogo = null;            // 
+    private String courseStyle = null;           // are credits counted
+    private String courseType = null;            // type of course
+
+	public Course() {		
+	}
+	public Course(int modules, String courseID, String  coursetype, String courseStyle) {
+	    this.modules = modules;
+	    this.courseID = courseID;
+	    this.courseType = coursetype;
+	    this.courseStyle = courseStyle;
+	}
+ 
+	public String getCourseLogo() {
+		return courseLogo;
+	}
+	public void setCourseLogo(String courseLogo) {
+		this.courseLogo = courseLogo;
+	}
+	public String getCourseMetadata() {
+		return courseMetadata;
+	}
+	public void setCourseMetadata(String courseMetadata) {
+		this.courseMetadata = courseMetadata;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+	public void setCourseStyle(String courseStyle) {
+		this.courseStyle = courseStyle;
+	}
+	public int getModules() {
+		return modules;
+	}
+	public String getCourseID() {
+		return courseID;
+	}
+	public String getCourseType() {
+		return courseType;
+	}
+	public String getCourseStyle() {
+		return courseStyle;
+	}
+	
 	/** Create new Course instance using the specified name and ID */
 	public Course(String name, String id) {	
 	}
@@ -20,4 +74,9 @@ public class Course {
     public String getID() {
     	return null;
     }
+    
+    public String toString(){
+    	return courseName + "(" + courseID +")";    	
+    }
+    
 }
