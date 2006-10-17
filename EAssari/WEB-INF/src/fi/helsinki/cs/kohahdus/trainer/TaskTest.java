3be31c093f366ffb67545a5c3735672dd8d9237d
Jari Inkinen <jjinkine@cs.helsinki.fi>
2006-10-17 09:39:29 +0000

    *** empty log message ***

---
+++ EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TaskTest.java
@@ -0,0 +1,203 @@
+package fi.helsinki.cs.kohahdus.trainer;
+
+import junit.framework.TestCase;
+
+public class TaskTest extends TestCase {
+	private Task taski;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		taski=new Task();
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.Task()'
+	 */
+	public void testTask() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.Task(String, String, String, String, String, String, String, String, boolean)'
+	 */
+	public void testTaskStringStringStringStringStringStringStringStringBoolean() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setLanguage(String)'
+	 */
+	public void testSetLanguage() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setName(String)'
+	 */
+	public void testSetName() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setAuthor(String)'
+	 */
+	public void testSetAuthor() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setDescription(String)'
+	 */
+	public void testSetDescription() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setModelAnswer(String)'
+	 */
+	public void testSetModelAnswer() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setCategory(String)'
+	 */
+	public void testSetCategory() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setTitoTaskType(String)'
+	 */
+	public void testSetTitoTaskType() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.SetFillInPreCode(String)'
+	 */
+	public void testSetFillInPreCode() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.SetFillInPostCode(String)'
+	 */
+	public void testSetFillInPostCode() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setFillInTask(boolean)'
+	 */
+	public void testSetFillInTask() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setValidateByModel(boolean)'
+	 */
+	public void testSetValidateByModel() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.setModificationDate()'
+	 */
+	public void testSetModificationDate() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getLanguage()'
+	 */
+	public void testGetLanguage() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getName()'
+	 */
+	public void testGetName() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getAuthor()'
+	 */
+	public void testGetAuthor() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getDescription()'
+	 */
+	public void testGetDescription() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getModelAnswer()'
+	 */
+	public void testGetModelAnswer() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getCategory()'
+	 */
+	public void testGetCategory() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getTitoTaskType()'
+	 */
+	public void testGetTitoTaskType() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getFillInPreCode()'
+	 */
+	public void testGetFillInPreCode() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getFillInPostCode()'
+	 */
+	public void testGetFillInPostCode() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.isFillInTask()'
+	 */
+	public void testIsFillInTask() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.isValidateByModel()'
+	 */
+	public void testIsValidateByModel() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getModificationDate()'
+	 */
+	public void testGetModificationDate() {
+
+	}
+
+	/*
+	 * Test method for 'fi.helsinki.cs.kohahdus.trainer.Task.getDateAsString()'
+	 */
+	public void testGetDateAsString() {
+		System.out.println(taski.getDateAsString());
+
+	}
+
+}
--- Misc/copy-jsp.sh
+++ Misc/copy-jsp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-scp -ri ~/rsa_id ~/ohtu/kohahdus/EAssari/WEB-INF/classes/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/EAssari/WEB-INF/classes/
-scp -ri ~/rsa_id ~/ohtu/kohahdus/EAssari/www/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/EAssari/www/
+scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/WEB-INF/classes/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/EAssari/WEB-INF/classes/
+scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/www/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/EAssari/www/
