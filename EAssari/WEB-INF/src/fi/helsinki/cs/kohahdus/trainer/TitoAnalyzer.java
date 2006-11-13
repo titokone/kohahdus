3c0022f7262fccb35728dd345e43524e74a0cff1
Jari Inkinen <jjinkine@cs.helsinki.fi>
2006-11-13 13:02:36 +0000

    *** empty log message ***

--- EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TitoAnalyzer.java
+++ EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TitoAnalyzer.java
@@ -6,9 +6,14 @@ import fi.helsinki.cs.kohahdus.criteria.TitoState;
 
 public class TitoAnalyzer {
 
+	public TitoAnalyzer() {
+	}
 	
 	
 	public TitoFeedback Analyze(Task task, String programCode, String keyboardInput) {
+		TitoState state=new TitoState();
+		state.
+		
 		return new TitoAnalyzerFeedback();
 	}
 	
