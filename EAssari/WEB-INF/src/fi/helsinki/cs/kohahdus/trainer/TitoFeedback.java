8c4388431a7c012e856b06a153c04404c5622b5d
Mikko Kinnunen <mkinnune@cs.helsinki.fi>
2006-11-13 13:10:01 +0000

    *** empty log message ***

--- EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TitoFeedback.java
+++ EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TitoFeedback.java
@@ -3,16 +3,22 @@ import fi.helsinki.cs.kohahdus.criteria.*;
 
 public interface TitoFeedback {
 	
+	/** Return the end-state of TitoKone run */
 	public TitoState getTitoState();
 
+	/** Return the overall task feedback */
 	public String getOverallFeedback();
 	
+	/** Return compile-time error message (null if no errors) */ 
 	public String getCompileError();
 	
+	/** Return run-time error message (null if no errors) */ 
 	public String getRunError();
 	
-	// TODO: Parempi nimi	
+	// TODO: Parempi nimi
+	/** Return true if program was compiled and executed successfully, and all acceptance crietia were met */ 
 	public boolean wasSuccessful();
 	
+	/** Return a list criteria feedbacks */ 
 	public java.util.List<TitoCriterionFeedback> getCriteriaFeedback();	
 }
