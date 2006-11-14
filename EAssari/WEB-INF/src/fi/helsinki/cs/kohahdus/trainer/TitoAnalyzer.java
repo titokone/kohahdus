2275d94379a0db0801176cb14317f05504a24cf1
Jari Inkinen <jjinkine@cs.helsinki.fi>
2006-11-14 13:09:52 +0000

    *** empty log message ***

--- EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TitoAnalyzer.java
+++ EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/trainer/TitoAnalyzer.java
@@ -1,8 +1,15 @@
 package fi.helsinki.cs.kohahdus.trainer;
 
+import java.util.HashMap;
 import java.util.List;
 
 import fi.helsinki.cs.kohahdus.criteria.*;
+
+/**
+ * 
+ * Author Ninja
+ * http://www.ebaumsworld.com/2006/06/jeopardy.html
+ */
 
 
 /** Class for running TitoKone and analyzing student's answer. */
@@ -17,17 +24,41 @@ public class TitoAnalyzer {
 	/** Analyzes student's answercode. Returns feedback from analysis. */
 	public TitoFeedback Analyze(Task task, List<Criterion> criteria, String programCode, String keyboardInput) {
 		
-		int maxInstructions=task.getMaximumNumberOfInstructions(); //get max instructions
-		state=new TitoState(); //creates new TitoState
-		state.compile(programCode); //compile the code
-		
+		//Create general data
+		int maxInstructions=task.getMaximumNumberOfInstructions();
+		state=new TitoState();
 		TitoAnalyzerFeedback feedback=new TitoAnalyzerFeedback();
 		
-		
-		String result=state.execute(keyboardInput, maxInstructions); //run the compiled program in TitoKone
-		if (!result.equals(null)) {
-			
+		//compile the code, check if errors
+		String compileResult=state.compile(programCode); //compile program with TitoKone
+		if (!compileResult.equals(null)) {
+			feedback.setCompileError(compileResult);
+			return feedback;
 		}
+		
+		//execute the successfully compiled code
+		String runResult=state.execute(keyboardInput, maxInstructions); //run the compiled program in TitoKone
+		if (!runResult.equals(null)) {
+			feedback.setRunError(runResult);
+			return feedback;
+		}
+		
+		//criterion check
+		int dfgsd=state.getRegister(int registerCode);
+		int wrerwe=state.getMemoryLocation(int address);
+		HashMap ewrrew=state.getSymbolTable();
+		
+		//allowed/forbidden instructions check
+		String usedInstructions[]=state.getUsedOpcodes();
+		
+		//quality criterion check
+		int ererw=state.getStackMaxSize();
+		int sdsdf=state.getExecutionSteps();
+		int sdfs=state.getCodeSize();
+		int sdfsd=state.getDataSize();
+		int sadfs=state.getMemoryAccessCount();
+		
+		//all systems clear
 		return feedback;
 	}
 	
@@ -42,6 +73,44 @@ public class TitoAnalyzer {
 		private String compileerror;
 		private String runerror;
 
+		//CONSTRUCTOR
+		public TitoAnalyzerFeedback(){
+		}
+		
+		//SET METHODS
+		
+		/** Set TitoState object */
+		public void setTitoState(TitoState state) {
+			titostate=state;
+		}
+		
+		/** Set overall feedback of this TitoAnalyzerFeedback */
+		public void setOverallFeedback(String overall) {
+			overallfeedback=overall;
+		}
+
+		/** Set boolean value if task was completed successfully */
+		public void setWasSuccessful(boolean value) {
+			wassuccess=value;
+		}
+		
+		/** Set list of CriterionFeedback objects */
+		public void setCriteriaFeedback(List<TitoCriterionFeedback> criteria) {
+			criterionfeedback=criteria;
+		}
+		
+		/** Set compile error */
+		public void setCompileError(String error) {
+			compileerror=error;
+		}
+		
+		/** Set run error. */
+		public void setRunError(String error) {
+			runerror=error;
+		}		
+		
+		//GET METHODS
+		
 		/** Return TitoState object */
 		public TitoState getTitoState() {
 			return null;
@@ -64,12 +133,14 @@ public class TitoAnalyzer {
 			// TODO Auto-generated method stub
 			return null;
 		}
-
+		
+		/** Return compile error */
 		public String getCompileError() {
 			// TODO Auto-generated method stub
 			return null;
 		}
-
+		
+		/** Return run error. */
 		public String getRunError() {
 			// TODO Auto-generated method stub
 			return null;
