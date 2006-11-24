3de1c3f67e75153d8d8c21d14fde305fe674adab
Paula Kuosmanen <pkkuosma@cs.helsinki.fi>
2006-11-24 11:29:45 +0000

    *** empty log message ***

--- Misc/copy-jsp.sh
+++ Misc/copy-jsp.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/WEB-INF/classes/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/EAssari/WEB-INF/classes/
-scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/www/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/EAssari/www/
-scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/xml/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/KONTEKSTI/WEB-INF/xml/
+scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/www/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/KONTEKSTI/www/
+scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/WEB-INF/xml/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/KONTEKSTI/WEB-INF/xml/
+scp -ri ~/rsa_id ~/workspace/kohahdus/EAssari/styles/* tkt_koha@db:/home/tkt_koha/tomcat/webapps/KONTEKSTI/styles/
