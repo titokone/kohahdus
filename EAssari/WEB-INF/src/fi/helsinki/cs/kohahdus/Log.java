25d2cdc5b2d0ef47e9d96affc6a0292c3cc05f58
Jari Inkinen <jjinkine@cs.helsinki.fi>
2006-12-12 12:01:05 +0000

    *** empty log message ***

--- EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/Log.java
+++ EAssari/WEB-INF/src/fi/helsinki/cs/kohahdus/Log.java
@@ -1,11 +1,16 @@
 package fi.helsinki.cs.kohahdus;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Log {
 	private static String context = "";
 	
 	public static void write(String data){
-		System.out.println(context + ": " + data);		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss ");
+		String aika = sdf.format(new Date());
+		System.out.println("[" + aika +"][" + context + "] " + data);		
 	}
 	
 	public static void write(Exception e){
